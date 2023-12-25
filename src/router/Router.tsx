5e76ff43234668ef0740236bad09380c82536a98
ThomasMumladze <mumladzea4@gmail.com>
2023-12-25 11:17:25 +0400

    App Router

---
+++ src/router/Router.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+
+import { Pages } from "../pages";
+import { Landing } from "../pages/Landing";
+
+const elementRoute = createRoutesFromElements(
+    <Route path="/" element={<Landing />}>
+        <Route index element={<Pages.Discover />} />
+        <Route path="/browse" element={<Pages.Browse />} />
+        <Route path="/distribution" element={<Pages.Distribution />} />
+    </Route>
+);
+
+const browserRoute = createBrowserRouter(elementRoute);
+
+export const AppRouter = () => {
+    const route = <RouterProvider router={browserRoute} />;
+
+    if (!route) return;
+
+    return route;
+};
