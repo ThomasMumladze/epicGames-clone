8648bccdd6f671c2c838933fbaafb108f9092f26
ThomasMumladze <mumladzea4@gmail.com>
2023-12-25 12:30:10 +0400

    update

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -4,16 +4,20 @@ import store_img from "../../assets/store.svg";
 
 import { Components } from "../../components";
 
-import { Constants } from "../../services";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
     return (
         <header className="w-[100%] bg-[#18181c] h-[4.5rem]">
             <div className="epic-shield"></div>
+
             <div className="header_body">
                 <div className="store">
-                    <img src={store_img} alt="" />
+                    <Link to={"/"}>
+                        <img src={store_img} alt="" />
+                    </Link>
                 </div>
+
                 <Components.Navigation />
             </div>
         </header>
