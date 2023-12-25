16cb7693ccd7fe16919cca413d3588a1263c2d20
ThomasMumladze <mumladzea4@gmail.com>
2023-12-25 13:01:40 +0400

    dropDawnMenu

---
+++ src/components/dropDawnMenu/languageMenu/LanguageMenu.tsx
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+export const LanguageMenu = () => {
+    return <div>LanguageMenu</div>;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,12 @@ import { Navigation } from "./Navigation/Navigation";
 export const Components = {
     Navigation,
 };
+
+// -- darop dawn menu
+import { EpicStoreMenu } from "./dropDawnMenu/epicStoreMenu/EpicStoreMenu";
+import { LanguageMenu } from "./dropDawnMenu/languageMenu/LanguageMenu";
+
+export const DropDawnMenu = {
+    EpicStoreMenu,
+    LanguageMenu,
+};
