a4582a42a2a45806f4d97e571f8c7921f3833072
ThomasMumladze <mumladzea4@gmail.com>
2024-01-08 11:08:02 +0400

    menu actions

--- src/components/dropDawnMenu/epicStoreMenu/EpicStoreMenu.tsx
+++ src/components/dropDawnMenu/epicStoreMenu/EpicStoreMenu.tsx
@@ -3,6 +3,7 @@ import "./style.scss";
 import { Constants } from "../../../services";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 interface constantNavType {
     title: string;
@@ -10,38 +11,44 @@ interface constantNavType {
 }
 
 export const EpicStoreMenu = () => {
+    const [menuOpen, setMenuOpen] = useState<boolean | null>(null);
     return (
         <div className="storeDropDawn">
             <div className="storeDropDawn-svg relative ">
-                <button className="flex items-center gap-[.5rem] w-[100%]">
+                <button
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    className="flex items-center gap-[.5rem] w-[100%]"
+                >
                     <div>{Constants.Svg.epicShield}</div>
                     <div className="arrowDawn">{Constants.Svg.arrowDawn}</div>
                 </button>
 
                 <div className="stick">{Constants.Svg.stick}</div>
 
-                <div className="menu-lists absolute top-20">
-                    <ul className="subMenu bg-[#202024]">
-                        {Constants.NavFlyoutStore.map((item: constantNavType, i) => (
-                            <li key={i}>
-                                <span className="text-[#FFF] capitalize">{item.title}</span>
-                                <ul>
-                                    {item.content.map((body, index) => (
-                                        <li key={index}>
-                                            <Link
-                                                className="capitalize"
-                                                to={body.url}
-                                                target={body.url == "/" ? "_self" : "_blank"}
-                                            >
-                                                {body.body}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                {menuOpen ? (
+                    <div className="menu-lists absolute top-20">
+                        <ul className="list-wrapper bg-[#202024]">
+                            {Constants.NavFlyoutStore.map((item: constantNavType, i) => (
+                                <li key={i}>
+                                    <span className="text-[#FFF] capitalize">{item.title}</span>
+                                    <ul>
+                                        {item.content.map((body, index) => (
+                                            <li key={index}>
+                                                <Link
+                                                    className="capitalize"
+                                                    to={body.url}
+                                                    target={body.url == "/" ? "_self" : "_blank"}
+                                                >
+                                                    {body.body}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ) : null}
             </div>
         </div>
     );
