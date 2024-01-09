afcf6d0ecd02b28c6b5135509e42ad503ae00837
ThomasMumladze <mumladzea4@gmail.com>
2024-01-09 15:38:38 +0400

    img src props add

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,11 +2,12 @@ import "./style.scss";
 
 interface Props {
     widthToDivision: string;
+    imgSrc: string;
 }
 export const Card = (props: Props) => {
     return (
         <div className="wrapper" style={{ width: `calc(100% / ${props.widthToDivision} - 16px)` }}>
-            <img src="https://cdn.wallpapersafari.com/67/82/U76fMj.jpg" alt="..." />
+            <img src={props.imgSrc} alt="..." />
         </div>
     );
 };
--- src/pages/Discover/Discover.tsx
+++ src/pages/Discover/Discover.tsx
@@ -11,9 +11,18 @@ export const Discover = () => {
                 {/* sales  epic coupon && featured giveaways*/}
                 <div className="sale-coupon-giveaways">
                     <div className="flex items-start justify-evenly w-[100%]">
-                        <Components.Card widthToDivision="3" />
-                        <Components.Card widthToDivision="3" />
-                        <Components.Card widthToDivision="3" />
+                        <Components.Card
+                            widthToDivision="3"
+                            imgSrc="https://cdn2.unrealengine.com/en-holiday-sale-3-up-breaker-1920x1080-e66be3535c99.jpg?h=480&quality=medium&resize=1&w=854"
+                        />
+                        <Components.Card
+                            widthToDivision="3"
+                            imgSrc="https://cdn2.unrealengine.com/en-coupon-3-up-asset-1920x1080-12f8c35fe87a.jpg?h=480&quality=medium&resize=1&w=854"
+                        />
+                        <Components.Card
+                            widthToDivision="3"
+                            imgSrc="https://cdn2.unrealengine.com/en-featured-giveaways-04-1920x1080-047f34358794.jpg?h=480&quality=medium&resize=1&w=854"
+                        />
                     </div>
                 </div>
                 {/* -------------------- END -------------------- */}
